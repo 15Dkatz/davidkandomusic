2158f837cbba4e901b0eeb8c1b2e94bf6f68471d
David Katz <15Dkatz@shcp.edu>
2023-06-01 11:26:24 -0700

    Complete quiz framework

--- src/app/quiz/data.js
+++ src/app/quiz/data.js
@@ -3,133 +3,162 @@ export const ROCKER = 'ROCKER';
 export const ROMANTIC = 'ROMANTIC';
 export const THINKER = 'THINKER';
 
+export const PREMISE = `Suddenly the sky cracks with a flash. A winged figure descends upon you. The figure introduces himself, "How's it hanging? I'm the Angel of Music. I have five questions for you. Complete them, and I'll give you a reward."`;
+
 export const QUESTIONS = [
   {
-    question: 'First, choose a music genre that represents you. Even if you haven’t heard of these whacky genres, pick whatever is speaking to you, or is sparking a feeling:',
+    id: 1,
+    question: `First, choose a music genre from the following - whatever is speaking to you, or sparking a feeling:`,
     answers: [
       {
         entry: 'a',
+        id: '1a',
         answer: 'Electric riff revolution',
         type: ROCKER
       },
       {
         entry: 'b',
+        id: '1b',
         answer: 'Heart-melting rhythmic rumble',
         type: ROMANTIC
       },
       {
         entry: 'c',
+        id: '1c',
         answer: 'Mind-blowing multi-instrumental',
         type: THINKER
       },
       {
         entry: 'd',
+        id: '1d',
         answer: 'Soul-soaring sonic',
         type: CRUISER
       }
     ]
   },
   {
-    question: 'Next, pick a musical creature from the spirit realm world to guide you:',
+    id: 2,
+    question: 'Next, pick a creature from the musical spirit realm world to guide you:',
     answers: [
       {
         entry: 'a',
-        answer: '"Mr. Butter" the Otter, the smooth synth operator',
+        id: '2a',
+        answer: '"Mr. Butter", the smooth synth-operating Otter',
         type: ROMANTIC
       },
       {
         entry: 'b',
-        answer: '"8-hands" the Octopus, the piano-playing aficionado',
+        id: '2b',
+        answer: '"8-hands", the piano-playing Octopus',
         type: THINKER
       },
       {
         entry: 'c',
-        answer: '"Freedom Feathers" the Eagle, the electric amp expert',
+        id: '2c',
+        answer: '"Flame Feathers", the guitar wizard Eagle',
         type: ROCKER
       },
       {
         entry: 'd',
-        answer: '"Groove Highway" the Stallion, the percussion professional',
+        id: '2d',
+        answer: '"Groove Highway", the percussion pro Pony',
         type: CRUISER
       }
     ]
   },
   {
-    question: 'Imagine you’ve decided to become a music artist. Come up with your performance alias:',
+    id: 3,
+    question: `Imagine you're becoming a music artist. Come up with your performance alias:`,
     answers: [
       {
         entry: 'a',
+        id: '3a',
         answer: 'The Sunwave',
         type: CRUISER
       },
       {
         entry: 'b',
+        id: '3b',
         answer: 'The Heartbender',
         type: ROMANTIC
       },
       {
         entry: 'c',
+        id: '3c',
         answer: 'The Epiphany',
         type: THINKER
       },
       {
         entry: 'd',
+        id: '3d',
         answer: 'The Thunder',
         type: ROCKER
       }
     ]
   },
   {
-    question: 'If you were an expert guitarist, which of the following guitar designs would you use?',
+    id: 4,
+    question: `Imagine you're an expert guitarist, which of the following guitar designs would you use?`,
     answers: [
       {
         entry: 'a',
+        id: '4a',
         answer: 'A V-shaped electric guitar with an angel-wing design',
         type: CRUISER
       },
       {
         entry: 'b',
-        answer: 'A rustic acoustic guitar with “this machine kills fascists“ written on it',
+        id: '4b',
+        answer: 'A rustic acoustic guitar with "this machine kills fascists" written on it',
         type: THINKER
       },
       {
         entry: 'c',
-        answer: 'A red double-neck electric guitar - you play twice as hard as average',
+        id: '4c',
+        answer: 'A silver double-neck electric guitar with neon strings',
         type: ROCKER
       },
       {
         entry: 'd',
-        answer: 'A finely-made acoustic guitar with pearl trim and roses engraved along the body',
+        id: '4d',
+        answer: 'A finely-made acoustic guitar with pearl trim',
         type: ROMANTIC
       }
     ]
   },
   {
-    question: 'Say you are an up-and-coming music artist. Choose one band for a week-long mentoring session:',
+    id: 5,
+    question: `Again, imageine you're an up-and-coming music artist. Choose one band for a week-long mentoring session:`,
     answers: [
       {
         entry: 'a',
+        id: '5a',
         answer: 'Queen',
         type: ROMANTIC
       },
       {
         entry: 'b',
+        id: '5b',
         answer: 'The Eagles',
         type: CRUISER
       },
       {
         entry: 'c',
+        id: '5c',
         answer: 'Led Zeppelin',
         type: ROCKER
       },
       {
         entry: 'd',
+        id: '5d',
         answer: 'The Beatles',
         type: THINKER
       }
     ]
   }
 ];
+
+export const CONCLUSION_PART_1 = `The Angel of Music cracks a smile, satisfied with your answers. "I dig your vibe." he says. "I made this playlist that I think represents you well. This is my gift to you."`
 
 export const RESULT_MAP = {
   [CRUISER]: {
@@ -141,7 +170,7 @@ export const RESULT_MAP = {
     playlistLink: 'https://open.spotify.com/playlist/1njREE782p2UmiVsuKQBu3?si=b57a0e3e7ce84849'
   },
   [ROMANTIC]: {
-    blurb: 'Your musical personality is warm and buttery. Listening to your music is like drinking the foam heart on a latte. Or it’s like a passionate embrace amidst an evening’s candlelight.',
+    blurb: `Your musical personality is warm and buttery. Listening to your music is like drinking the foam heart on a latte. Or it's like a passionate embrace amidst an evening's candlelight.`,
     playlistLink: 'https://open.spotify.com/playlist/3TMHZHFF7hlQbFYCWqbHrt?si=ceaff9f649ba4983'
   },
   [THINKER]: {
@@ -150,8 +179,69 @@ export const RESULT_MAP = {
   }
 };
 
-// TODO: Return a result from the result map based on the selections. (tally up the player's selections)
-// If there is a tie (2-2-1), then choose a random one from the selections with 2 tallies.
-export const GetQuizResult = ({ selections }) => {
+export const CONCLUSION_PART_2 = `Once again, the sky cracks with a flash. The Angel of Music begins to ascend, "Until we meet again. Hope you enjoy the playlist. Oh, and don't forget to follow my favorite artist on Spotify: David Kando. For real, that guy can do music."`;
 
+// If there is a tie (2-2-1), then choose a random one from the selections with 2 tallies.
+// selections should be a map like so:
+// {
+//   [CRUISER]: numVotes,
+//   [ROCKER]: numVotes,
+//   [ROMANTIC]: numVotes,
+//   [THINKER]: numVotes
+// }
+// returns the type, such as CRUISER or ROCKER
+export const getQuizResult = ({ resultMap }) => {
+  // 1: find the max
+  const max = Math.max(...Object.values(resultMap));
+
+  // 2: filter the list down to selections that match the max (go directly to the keys, since those are the types)
+  const filteredKeys = Object.keys(
+    Object.fromEntries(
+      Object.entries(resultMap).filter(([_, numVotes]) => numVotes === max)
+    )
+  );
+
+  // 3: pick a random item from the list
+  return filteredKeys[Math.floor(Math.random() * filteredKeys.length)];
 }
+
+// Uncomment for tests
+// const test1 = getQuizResult({
+//   resultMap: {
+//     [CRUISER]: 1,
+//     [ROCKER]: 1,
+//     [ROMANTIC]: 1,
+//     [THINKER]: 2
+//   }
+// });
+// console.log(`test1`, test1);
+
+// const test2 = getQuizResult({
+//   resultMap: {
+//     [CRUISER]: 1,
+//     [ROCKER]: 2,
+//     [ROMANTIC]: 0,
+//     [THINKER]: 2
+//   }
+// });
+// console.log(`test2`, test2);
+
+// const test3 = getQuizResult({
+//   resultMap: {
+//     [CRUISER]: 1,
+//     [ROCKER]: 3,
+//     [ROMANTIC]: 0,
+//     [THINKER]: 1
+//   }
+// });
+// console.log(`test3`, test3);
+
+// const test4 = getQuizResult({
+//   resultMap: {
+//     [CRUISER]: 5,
+//     [ROCKER]: 0,
+//     [ROMANTIC]: 0,
+//     [THINKER]: 0
+//   }
+// });
+// console.log(`test4`, test4);
--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,16 +1,146 @@
 'use client';
 
+import { useState, useRef, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
 import PageLayout from '../components/pageLayout';
+import {
+  PREMISE,
+  QUESTIONS,
+  RESULT_MAP,
+  CONCLUSION_PART_1,
+  CONCLUSION_PART_2,
+  getQuizResult
+} from './data';
+
+function QuizForm() {
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const toggleDisplayForm = () => setDisplayForm(!displayForm);
+
+  return (
+    <div>
+      {PREMISE}
+      {
+        displayForm ? (
+          <div>
+            <form>
+              {
+                QUESTIONS.map(
+                  ({ id: questionId, question, answers }) => (
+                    <div className="mt-5" key={questionId}>
+                      <fieldset>
+                        <legend>{questionId}: {question}</legend>
+                        {
+                          answers.map(({ id: answerId, answer, type }) => (
+                            <div key={answerId}>
+                              <input id={answerId} type="radio" name={questionId} value={type} required />
+                              <label htmlFor={answerId}>{' '}{answer}</label>
+                            </div>
+                          ))
+                        }
+                      </fieldset>
+                    </div>
+                  )
+                )
+              }
+              <div className="text-center">
+                <button
+                  className="rounded-none bg-blue-700 text-white p-2 mt-5"
+                  // then useSearchParams to get the result.
+                >
+                  Submit and get my gift
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div className="text-center mt-5">
+            <button
+              className="rounded-none bg-blue-700 text-white p-2"
+              onClick={toggleDisplayForm}
+            >
+              Ok, let's do this.
+            </button>
+          </div>
+        )
+      }
+    </div>
+  )
+}
 
 export default function Quiz() {
+  const resultsRef = useRef(null);
+  const searchParams = useSearchParams();
+  const params = searchParams.toString();
+
+  let displayResults = params.length > 0;
+
+  let Results = () => <></>;
+
+  // TODO: Scroll to result manually since the entire page is re-rendered on submit
+  if (displayResults) {
+    // TODO: If malformed params, set displayResults to false manually to start over
+    // split params by &, and grab the values after the = sign
+    const gatheredSelections = params.split('&').map(param => param.match(/=(.*)/)[1]);
+    const resultMap = {};
+
+    gatheredSelections.forEach(selection => {
+      if (!resultMap[selection]) {
+        resultMap[selection] = 1;
+      } else {
+        resultMap[selection] = resultMap[selection]+1;
+      }
+    });
+
+    const result = getQuizResult({ resultMap });
+
+    const { blurb, playlistLink } = RESULT_MAP[result];
+
+    Results = () => (
+      <div ref={resultsRef}>
+        <div>
+          {CONCLUSION_PART_1}
+        </div>
+        <br />
+        <div>
+          "{blurb}"
+          {/* {playlistLink} */}
+        </div>
+        <br />
+        <div>
+          {CONCLUSION_PART_2}
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    if (displayResults && resultsRef.current) {
+      resultsRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+
   return (
     <PageLayout
       // TODO: Consider a linear gradient or image background
       // background="bg-[url('/goofy_bg_500_op_80.png')] bg-contain"
+      background="bg-gradient-to-b from-slate-100 to-green-50"
       title="Playlist Personality Quiz"
     >
-      <div className={"text-md lg:text-xl p-1"}>
-        Take the quiz. Get your playlist.
+      <div className={"text-md lg:text-xl p-1 m-1"}>
+        <div className="mt-2 md:mt-10">
+          {
+            displayResults ? (
+              <Results />
+            ) : (
+              <QuizForm />
+            )
+          }
+        </div>
+        <br />
+        {/* Hide results and conclusion until form submission */}
+        {/* Ensure that all questions have been answered */}
       </div>
     </PageLayout>
   )
