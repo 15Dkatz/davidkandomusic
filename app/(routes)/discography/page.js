5422ca79ee78b93e6d502c27c95fad97dec7b84f
David Katz <15Dkatz@shcp.edu>
2023-06-09 21:16:26 -0700

    Implement useDeferredValue

--- app/(routes)/discography/page.js
+++ app/(routes)/discography/page.js
@@ -1,12 +1,9 @@
+'use client';
+
+import { useDeferredValue, useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-
-import PageStructure from 'app/components/page-structure';
 import { RECORDS } from '../record/[id]/data';
-
-export const metadata = {
-  title: 'Discography - David Kando'
-};
 
 function PlayRow({ title, link, iconImage }) {
   return (
@@ -25,59 +22,119 @@ function PlayRow({ title, link, iconImage }) {
   )
 }
 
-export default function Discography() {
-  return (
-    <PageStructure
-      background="bg-gradient-to-b from-slate-100 to-blue-100"
-      title="Discography"
-    >
-      {
-        RECORDS.map(({
-          id,
-          title,
-          date,
-          blurb,
-          albumImage,
-          recordHalfImage,
-          spotifyLink,
-          appleMusicLink,
-          youtubeMusicLink
-        }, i) => {
-          const addedMb = i < RECORDS.length ? " mb-5 md:mb-10" : "";
+// use useDeferredValue here, and have a search bar.
+// Highlight the matching text in the record title.
 
-          return (
-            <div key={title} className={`w-full bg-white p-5 md:p-10 ${addedMb}`}>
-              <div className="flex flex-row justify-center">
-                <Link
-                  className={
-                    `relative w-[180px] h-[180px] md:w-[280px] md:h-[280px]
-                    border-black border-[2px]`
-                  }
-                  href={`/record/${id}`}
-                >
-                  <Image src={albumImage} alt="album image" fill />
-                </Link>
-                <div className={"relative w-[90px] h-[180px] md:w-[140px] md:h-[280px]"}>
-                  <Image src={recordHalfImage} alt="album image" fill />
+const titleDisplay = ({ deferredText, title }) => {
+  if (
+    deferredText.length > 0
+    && title.toLowerCase().includes(deferredText.toLowerCase())
+  ) {
+    const splitTitle = title.toLowerCase().split(deferredText.toLowerCase());
+    // console.log(`splitTitle`, splitTitle);
+
+    // Don't return the splits directly, need to preserve capitalization
+    const title1 = title.slice(0, splitTitle[0].length);
+    const title2 = title.slice(splitTitle[0].length, splitTitle[0].length+deferredText.length);
+    const title3 = title.slice(splitTitle[0].length+deferredText.length, title.length);
+    // console.log(`title1`, title1);
+    // console.log(`title2`, title2);
+    // console.log(`title3`, title3);
+
+    return (
+      <>
+        <span>{title1}</span>
+        <span className="font-bold text-blue-800">{title2}</span>
+        <span>{title3}</span>
+      </>
+    )
+  }
+
+  return <span>{title}</span>;
+}
+
+export default function Discography() {
+  const [text, setText] = useState('');
+  const [records, setRecords] = useState(RECORDS);
+  const deferredText = useDeferredValue(text);
+  // console.log(`deferredText`, deferredText);
+
+  useEffect(() => {
+    const filteredRecords = RECORDS.filter(
+      record => record.title.toLowerCase().includes(deferredText.toLowerCase())
+    );
+
+    setRecords(deferredText.length > 0 ? filteredRecords : RECORDS);
+  }, [deferredText]);
+
+  return (
+    <>
+      <div className="w-[330px] md:w-[500px] bg-white p-5 md:p-10 text-center">
+        <label>
+          <div className="mb-2">Search for a record</div>
+          <input
+            type="text"
+            className="rounded-none w-full border-slate-400 border-[2px]"
+            value={text}
+            onChange={event => setText(event.target.value)}
+          />
+        </label>
+      </div>
+      <br />
+      {
+        records.length > 0 ? (
+          records.map(({
+            id,
+            title,
+            date,
+            blurb,
+            albumImage,
+            recordHalfImage,
+            spotifyLink,
+            appleMusicLink,
+            youtubeMusicLink
+          }, i) => {
+            return (
+              <>
+                <div key={title} className="w-[330px] md:w-[500px] bg-white p-5 md:p-10">
+                  <div className="flex flex-row justify-center">
+                    <Link
+                      className={
+                        `relative w-[180px] h-[180px] md:w-[280px] md:h-[280px]
+                        border-black border-[2px]`
+                      }
+                      href={`/record/${id}`}
+                    >
+                      <Image src={albumImage} alt="album image" fill />
+                    </Link>
+                    <div className={"relative w-[90px] h-[180px] md:w-[140px] md:h-[280px]"}>
+                      <Image src={recordHalfImage} alt="album image" fill />
+                    </div>
+                  </div>
+                  <div className="mb-5 mt-5">
+                    <div className="font-bold text-base md:text-xl text-left">
+                      {titleDisplay({ deferredText, title })}
+                    </div>
+                    <div className="text-justify md:text-lg mb-1">{blurb}</div>
+                    <div className="text-sm text-slate-600">
+                      Release date: {date}
+                    </div>
+                  </div>
+                  <div className="md:m-2"></div>
+                  <PlayRow title={'Spotify'} link={spotifyLink} iconImage='/spotify_icon_w500.png' />
+                  <PlayRow title={'Apple Music'} link={appleMusicLink} iconImage='/apple_music_icon_w500.png' />
+                  <PlayRow title={'YouTube Music'} link={youtubeMusicLink} iconImage='/youtube_music_icon_w500.png' />
                 </div>
-              </div>
-              <div className="mb-5 mt-5">
-                <div className="font-bold text-base md:text-xl text-left">
-                  {title}
-                </div>
-                <div className="text-justify md:text-lg mb-1">{blurb}</div>
-                <div className="text-sm text-slate-600">
-                  Release date: {date}
-                </div>
-              </div>
-              <div className="md:m-2"></div>
-              <PlayRow title={'Spotify'} link={spotifyLink} iconImage='/spotify_icon_w500.png' />
-              <PlayRow title={'Apple Music'} link={appleMusicLink} iconImage='/apple_music_icon_w500.png' />
-              <PlayRow title={'YouTube Music'} link={youtubeMusicLink} iconImage='/youtube_music_icon_w500.png' />
-            </div>
-          )
-        })
+                {i < RECORDS.length ? <br /> : <></>}
+              </>
+            )
+          })
+        ) : (
+          <div className="w-[330px] h-[500px] md:w-[500px] md:h-[650px] bg-white p-5 md:p-10">
+            <div className="text-center">No match.</div>
+          </div>
+        )
       }
-    </PageStructure>
+    </>
   )
 }
