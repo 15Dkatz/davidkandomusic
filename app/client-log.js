7c0dd1ab3395b0a118da1fe4711dbf32aa139555
David Katz <15Dkatz@shcp.edu>
2023-06-27 11:02:47 -0700

    Fix client log indentation

--- app/client-log.js
+++ app/client-log.js
@@ -7,12 +7,12 @@ export default function ClientLog() {
 
   useEffect(() => {
     console.log(`
-    Hey there coder, digging into the website? Well, you've found an easter egg!
-    I actually posted a step-by-step course on rebuilding this website.
-    It uses Next.js 13+ and React 18+.
-    Take the course, and adapt this website for yourself or a client!
-    Check out the course here: https://www.udemy.com/user/54cd8dd54e49b/
-  `);
+      Hey there coder, digging into the website? Well, you've found an easter egg!
+      I actually posted a step-by-step course on rebuilding this website.
+      It uses Next.js 13+ and React 18+.
+      Take the course, and adapt this website for yourself or a client!
+      Check out the course here: https://www.udemy.com/user/54cd8dd54e49b/
+    `);
   }, [])
 
   return <></>;
