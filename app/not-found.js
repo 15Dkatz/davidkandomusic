65fc6f178df972e2f381b1f415925f456cefb1bd
David Katz <15Dkatz@shcp.edu>
2023-07-07 23:24:24 -0700

    Simplify not found

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col">
+    <div>
       <div className="font-ranga text-4xl">404 - Page Not Found.</div>
       {/* <div className="font-roboto">Try going to the <a className="link" href="/">home page</a>.</div> */}
       <div className="font-roboto text-2xl">Try going to the <Link className="link" href="/">home page</Link>.</div>
